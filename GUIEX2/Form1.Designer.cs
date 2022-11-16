74fc7271c2989e710c54fc035fea34de1db34967
waffl <waffl@DESKTOP-S1OKJOC>
2022-11-15 19:30:48 -0800

    Add project files.

---
+++ GUIEX2/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace GUIEX2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.TotalResinCounter = new System.Windows.Forms.Timer(this.components);
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.TimeTillResinIsDone = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.label1.Location = new System.Drawing.Point(343, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Resin";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox1.Location = new System.Drawing.Point(434, 251);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 397);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 70);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Start Timer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // TotalResinCounter
+            // 
+            this.TotalResinCounter.Interval = 480000;
+            this.TotalResinCounter.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox3.Location = new System.Drawing.Point(647, 282);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(253, 68);
+            this.textBox3.TabIndex = 18;
+            this.textBox3.Text = "00:00:00";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Visible = false;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox4.Location = new System.Drawing.Point(201, 282);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(253, 68);
+            this.textBox4.TabIndex = 19;
+            this.textBox4.Text = "0/160";
+            this.textBox4.Visible = false;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // TimeTillResinIsDone
+            // 
+            this.TimeTillResinIsDone.Interval = 1000;
+            this.TimeTillResinIsDone.Tick += new System.EventHandler(this.TimeTillResinIsDone_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(459, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 77);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(647, 397);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 70);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Quit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-14, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1101, 624);
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(490, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 68);
+            this.button4.TabIndex = 23;
+            this.button4.Text = "Minimize";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(191)))), ((int)(((byte)(231)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1087, 620);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "GUIEX";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(120)))), ((int)(((byte)(200)))));
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer TotalResinCounter;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Timer TimeTillResinIsDone;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ GUIEX2/Form1.cs
@@ -0,0 +1,199 @@
+﻿//stuff that still needs to be done
+//figure out how to make the picture box dragable
+//design
+//  Button customization
+//  Text within the userinput
+//  color of background
+//  see if you can change the shape of the total resin box and timer box
+//  
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace GUIEX2
+{
+
+    public partial class Form1 : Form
+    {
+        private bool _dragging = false;
+        private Point _offest;
+        private Point _start_point = new Point(0,0);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)//this is the start button
+        {
+            string currentResin = textBox1.Text;
+            int currentResinValue = int.Parse(currentResin);
+            if (currentResinValue > 160)
+            {
+                currentResinValue = 160;
+            }
+            else if (currentResinValue < 0)
+            {
+                currentResinValue = 0;
+            }
+            currentResin = currentResinValue.ToString();
+            textBox4.Visible = true; //used to interact with the total resin box
+            textBox4.Text = currentResin + "/160";
+            TotalResinCounter.Enabled = true;
+
+            int resinTimer = currentResinValue * 8; 
+            resinTimer = 1280 - resinTimer;
+            int resinMins = resinTimer % 60;
+            int resinHours = resinTimer / 60;
+            textBox3.Visible = true; //used to interact with the time left box
+            textBox3.Text = resinHours.ToString() + ":" + resinMins.ToString() + ":00";
+            TimeTillResinIsDone.Enabled = true;
+
+            button1.Visible = true;
+            button4.Visible = true;
+
+            label1.Visible = false; // removes the orginal layout for the timer and the total resin
+            textBox1.Visible = false;
+            button3.Visible = false;
+            button2.Visible = false;
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)//timer for the total resin count
+        {
+            string var = textBox4.Text;
+            char[] seperator = { '/', ' ' };
+            int count = 2;
+            string[] strings = var.Split(seperator, count, StringSplitOptions.None);
+            int currentResinValue = int.Parse(strings[0]);
+            currentResinValue += 1;
+            string strResinValue = currentResinValue.ToString();
+            string combinedResinValue = strResinValue + "/" + strings[1];
+            if (currentResinValue <= 160)
+            {
+                textBox4.Text = combinedResinValue;
+            }
+            else { TotalResinCounter.Enabled = false; }           
+        }
+
+        private void TimeTillResinIsDone_Tick(object sender, EventArgs e)//timer for the countdown timer
+        {
+            int s, m, h;
+            bool timeron = true;
+            string var = textBox3.Text;
+            char[] seperator = { ':', ' ' };
+            int count = 3;//look here if there are any problems
+            string[] strings = var.Split(seperator, count, StringSplitOptions.None);
+            s = int.Parse(strings[2]);
+            m = int.Parse(strings[1]);
+            h = int.Parse(strings[0]);
+            if (h == 0 && m == 0 && s == 0)
+            {
+                TimeTillResinIsDone.Enabled = false;
+                timeron = false;
+            }
+            if (timeron)
+            {
+                if (s <= 0)
+                {
+                    m--;
+                    s = 60;
+                }
+                if (m <= 0)
+                {
+                    h--;
+                    m = 59;
+                }
+                s--;
+                string newtime = h.ToString() + ":" + m.ToString() + ":" + s.ToString();
+                textBox3.Text = newtime;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)//the reset button
+        {
+            textBox4.Visible = false;
+            TotalResinCounter.Enabled = false;
+            textBox3.Visible = false; 
+            TimeTillResinIsDone.Enabled = false;
+            button1.Visible = false;
+            button4.Visible = false;
+
+            label1.Visible = true;
+            textBox1.Visible = true;
+            button3.Visible = true;
+            button2.Visible = true;
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;
+            _start_point = new Point(e.X,e.Y);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+    }
+}
